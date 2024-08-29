09bf3d3eabc89cdf1bd938f044966224f7ffe7e3
Tomal162 <tomal.dhar.bgc@gmail.com>
2024-08-30 00:03:41 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    darkMode: 'class', // or 'media' if you want to use the system's preference
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  };
